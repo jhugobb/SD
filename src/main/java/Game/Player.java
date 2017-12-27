8620ae753a5f5bec5cdc504c2bd57bfc998b6d40
Resende <j.p.resende68@gmail.com>
2017-12-27 00:54:22 +0000

    classes

---
+++ src/main/java/Game/Player.java
@@ -0,0 +1,9 @@
+package Game;
+
+public class Player {
+    private String username;
+    private String password;
+    private Integer rank;
+    private Integer wins;
+    private Integer losts;
+}
