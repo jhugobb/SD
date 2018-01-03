13c78c5a7e7aba08a679ea3561f1b5d9d096e169
jhugobb <hugo.b.brandao@gmail.com>
2018-01-03 17:10:44 +0000

    Implement queues to negate starvation

--- src/main/java/Server/Game/Engine.java
+++ src/main/java/Server/Game/Engine.java
@@ -4,16 +4,13 @@ import Server.Exceptions.InvalidAuthenticationException;
 import Server.Exceptions.PlayerAlredyExistsException;
 import Server.Hub;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 public class Engine {
-    private HashMap<Integer, Set<String>> queue;
+    private HashMap<Integer, Queue<String>> queue;
     private HashMap<String,Player> players;
     private Lock playerLock;
     private Lock queueLock;
@@ -23,7 +20,7 @@ public class Engine {
         this.queueLock = new ReentrantLock();
         this.queue = new HashMap<>();
         for (int i = 0; i < 10; i++)
-            this.queue.put(i, new HashSet<>());
+            this.queue.put(i, new LinkedList<>());
         this.players = new HashMap<>();
    }
 
@@ -70,8 +67,8 @@ public class Engine {
         }
         queueLock.lock();
         try {
-            Set<String> rankQueue = this.queue.get(rank);
-            rankQueue.add(player.getUsername());
+            Queue<String> rankQueue = this.queue.get(rank);
+            rankQueue.offer(player.getUsername());
             if (weHaveAParty(rank)) {
                 playersID = this.getAParty(rank);
             }
@@ -100,8 +97,8 @@ public class Engine {
 
     private Boolean weHaveAParty(Integer rank) {
         Integer size = queue.get(rank).size();
-        Set<String> above = queue.get(rank+1);
-        Set<String> below = queue.get(rank-1);
+        Queue<String> above = queue.get(rank+1);
+        Queue<String> below = queue.get(rank-1);
         if (above != null) size+= above.size();
         if (below != null) size+=below.size();
         return size >= 10;
@@ -109,20 +106,19 @@ public class Engine {
 
     private Set<String> getAParty(Integer rank) {
         Set<String> res = new HashSet<>();
-        Set<String> queueRank = queue.get(rank);
-        Set<String> above = queue.get(rank+1);
-        Set<String> below = queue.get(rank-1);
+        Queue<String> queueRank = queue.get(rank);
+        Queue<String> above = queue.get(rank+1);
+        Queue<String> below = queue.get(rank-1);
         getPlayers(res, queueRank);
         getPlayers(res, above);
         getPlayers(res, below);
         return res;
     }
 
-    private void getPlayers(Set<String> res, Set<String> rank) {
+    private void getPlayers(Set<String> res, Queue<String> rank) {
         if (rank != null) {
-            Iterator<String> it = rank.iterator();
-            while (res.size() < 10 && it.hasNext()) {
-                res.add(it.next());
+            while (res.size() < 10 && !rank.isEmpty()) {
+                res.add(rank.poll());
             }
             rank.removeAll(res);
         }
